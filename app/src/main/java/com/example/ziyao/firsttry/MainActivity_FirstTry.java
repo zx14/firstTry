3a050f29bf2c7b9e7ad51c4003bb278ed7e45df1
zx14 <ziyao_xu@brown.edu>
2017-11-12 17:00:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ziyao/firsttry/MainActivity_FirstTry.java
@@ -0,0 +1,13 @@
+package com.example.ziyao.firsttry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_FirstTry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__first_try);
+    }
+}
