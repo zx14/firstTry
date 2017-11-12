45605f4af7647501cac8c8c968d2d3d163f87217
zx14 <ziyao_xu@brown.edu>
2017-11-12 17:07:49 -0500

    vcs update

--- app/src/main/java/com/example/ziyao/firsttry/MainActivity_FirstTry.java
+++ app/src/main/java/com/example/ziyao/firsttry/MainActivity_FirstTry.java
@@ -8,7 +8,6 @@ public class MainActivity_FirstTry extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main__first_try);
     }
 }
